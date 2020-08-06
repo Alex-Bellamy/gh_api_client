aa84468fb4c11e375809fd5724f1af863b539617
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-06 16:25:57 +0200

    tidying up existing file scaffold and formatting

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import React from "react";
+import { Button, Input } from "semantic-ui-react";
 
 const GHSearch = () => {
   return (
     <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
-      <Button id="submit" name="search">Search</Button>
+      <Input type="text" name="search" placeholder="Input GH username" />
+      <Button id="submit" name="search">
+        Search
+      </Button>
     </>
-  )
-}
+  );
+};
 
-export default GHSearch
+export default GHSearch;
