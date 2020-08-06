fb929234bd052814aba297cdd1fcb1ecbafc7f52
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-06 16:55:55 +0200

    installing and importing axios, converting GHSearch to class component and assigning functions to search facility

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import { Button, Input } from "semantic-ui-react";
+import axios from "axios";
 
-const GHSearch = () => {
-  return (
-    <>
-      <Input type="text" name="search" placeholder="Input GH username" />
-      <Button id="submit" name="search">
-        Search
-      </Button>
-    </>
-  );
-};
+class GHSearch extends Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            type="text"
+            name="search"
+            placeholder="Input GH username"
+            value={this.State.searchTerm}
+            onChange={this.onChange}
+          />
+          <Button id="submit" name="search">
+            Search
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default GHSearch;
